dda4655c8f50031e961bd78a5c386c33224a9397
li1115 <598435866@qq.com>
2022-08-14 21:03:25 -0400

    web222

--- js/main.js
+++ js/main.js
@@ -11,7 +11,6 @@ const getValueInput = () =>{
     let reg = /^[A-Za-z]\d[A-Za-z][\s-]?\d[A-Za-z]\d$/;
     if (reg.test(inpt)) document.getElementById('valueInput').innerHTML = '';
     else document.getElementById('valueInput').innerHTML = 'Check your inputs and try again';
-    // document.getElementById('valueInput').innerHTML = inpt
 
     let phone = document.getElementById('phone').value;
     let r = /^((\(\d{3}\))|(\d{3}))\s?\d{3}[\s-]?\d{4}$/;
@@ -19,29 +18,6 @@ const getValueInput = () =>{
     else document.getElementById('value2Input').innerHTML = 'Check your inputs and try again'
 }
 
-// const box = document.getElementById('active')
-
-// function handleClick(){
-
-//     var chk = document.getElementById('hiring');
-//     var active = document.getElementById('active');
-
-//     let html= ```
-//     <label for="rate">Hourly Rate</label>
-//     <input type="text" name="rate">```
-
-//     if(chk.checked){
-//         // active.append(html)
-//         console.log('clicked')
-
-//     }
-    // if (document.getElementById('hiring').checked){
-    //     box.classList.to= 'active';
-    // }
-    // else{
-    //     box.style.display = 'none';
-    // }
-// }
 
 function showInput(){
     let rate = document.getElementById('active');
